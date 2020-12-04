f7a15d66d5c99792d0dd81ca45e45d527e5ebe5e
Igboanugwo Collins <munisco12@gmail.com>
2020-12-04 23:45:19 +0100

    Fetched countries

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,22 @@
-const Home = () => {
+const Home = ({countries}) => {
+    console.log('the coutries', countries)
     return (
         <div>
             The Home
         </div>
     )
+}
+
+export const getStaticProps = async () => {
+    const req = await fetch('https://restcountries.eu/rest/v2/all')
+    const res = await req.json()
+
+    return {
+        props: {
+            countries: res
+        }
+    }
+    
 }
 
 export default Home
